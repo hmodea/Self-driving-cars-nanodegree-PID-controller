39a1cdb1226b3e136d5eb403c5296cb14e510c93
hmodea <haytham.modea@gmail.com>
2019-12-15 20:21:40 +0100

    Add PID implementation

---
+++ Self-driving-cars-nanodegree-PID-controller/src/PID.cpp
@@ -0,0 +1,30 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  
+    Kp = Kp_;
+    Ki = Ki_;
+    Kd = Kd_;
+  
+}
+
+void PID::UpdateError(double cte) {
+  
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
+
+}
+
+double PID::TotalError() {
+  
+  return (Kp * p_error + Kd * d_error + Ki * i_error);
+}
